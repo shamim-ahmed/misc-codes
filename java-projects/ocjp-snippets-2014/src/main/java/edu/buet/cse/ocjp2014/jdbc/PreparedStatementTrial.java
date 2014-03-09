4d3224876affbc23ee681129b358124454a91ca5
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2014-03-09 16:21:44 +1100

    added an example on using a PreparedStatement

--- java-projects/ocjp-snippets-2014/src/main/java/edu/buet/cse/ocjp2014/jdbc/PreparedStatementTrial.java
+++ java-projects/ocjp-snippets-2014/src/main/java/edu/buet/cse/ocjp2014/jdbc/PreparedStatementTrial.java
@@ -4,21 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
-public class DbConnectionTrial {
+import java.sql.PreparedStatement;
+
+public class PreparedStatementTrial {
   public static void main(String... args) {
-    Properties authProps = new Properties();
-    authProps.put("user", "root");
-    authProps.put("password", "admin");
+    Properties dbProps = new Properties();
+    dbProps.put("user", "root");
+    dbProps.put("password", "admin");
     final String dbUrl = "jdbc:mysql://localhost:3306/ocjp_db";
-    final String sqlQuery = "SELECT * FROM User";
-
-    try (Connection conn = DriverManager.getConnection(dbUrl, authProps); 
-         Statement stmt = conn.createStatement()) {
-      ResultSet resultSet = stmt.executeQuery(sqlQuery);
-
+    final String sqlQuery = "SELECT * FROM User WHERE user_id = ?";
+    
+    try (Connection conn = DriverManager.getConnection(dbUrl, dbProps);
+         PreparedStatement stmt = conn.prepareStatement(sqlQuery)) {
+      stmt.setInt(1, 1);
+      ResultSet resultSet = stmt.executeQuery();
+      
       while (resultSet.next()) {
         System.out.printf("%s\t%s\t%s%n", resultSet.getInt("user_id"), resultSet.getString("username"),
             resultSet.getString("email"));
