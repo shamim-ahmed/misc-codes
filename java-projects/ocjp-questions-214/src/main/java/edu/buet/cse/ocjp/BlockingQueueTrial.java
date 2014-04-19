8baf4fa6ca63abf8dc243db3da4b931d8cc5ad6a
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2014-04-19 19:36:16 +1000

    added a new project

---
+++ java-projects/ocjp-questions-214/src/main/java/edu/buet/cse/ocjp/BlockingQueueTrial.java
@@ -0,0 +1,16 @@
+package edu.buet.cse.ocjp;
+
+public class BlockingQueueTrial {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+class Producer implements Runnable {
+  
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+    
+  }
+}
