dccd6a7483a21d48734accd2f23fd9e652dcea84
Shamim Ahmed <shamim.buet.99@gmail.com>
2014-08-30 18:29:58 +1000

    added my first java 8 program

---
+++ java-projects/java8-in-action/src/main/java/edu/buet/cse/App.java
@@ -0,0 +1,13 @@
+package edu.buet.cse;
+
+import java.io.File;
+
+public class App {
+  public static void main(String[] args) {
+    File[] hiddenFiles = new File(".").listFiles(File::isHidden);
+    
+    for (File f : hiddenFiles) {
+      System.out.println(f);
+    }
+  }
+}
