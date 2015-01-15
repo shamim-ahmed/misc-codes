ba99958dbec10084c6a115059140de7f09d0e73d
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-01-15 23:35:44 +1100

    added examples on updating and deleting a retrieved entity

---
+++ java-projects/jpa-certification/jpa-ch01-v1/src/main/java/edu/buet/cse/jpa/ch01/v1/driver/App4.java
@@ -0,0 +1,40 @@
+package edu.buet.cse.jpa.ch01.v1.driver;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import edu.buet.cse.jpa.ch01.v1.Vehicle;
+
+public class App4 {
+  public static void main(String... args) {
+    EntityManagerFactory factory = null;
+    EntityManager em = null;
+    
+    try {
+      factory = Persistence.createEntityManagerFactory("jpa-ch01-v1");
+      em = factory.createEntityManager();
+      
+      TypedQuery<Vehicle> query = em.createQuery("SELECT v FROM Vehicle v WHERE v.vin LIKE '%123%'", Vehicle.class);
+      Vehicle vehicle = query.getSingleResult();
+      
+      em.getTransaction().begin();
+      vehicle.setYear(2015);
+      em.persist(vehicle);
+      em.getTransaction().commit();
+      
+      System.out.printf("%s was updated successfully%n", vehicle);
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
+    } finally {
+      if (em != null) {
+        em.close();
+      }
+      
+      if (factory != null) {
+        factory.close();
+      }
+    }
+  }
+}
---
+++ java-projects/jpa-certification/jpa-ch01-v1/src/main/java/edu/buet/cse/jpa/ch01/v1/driver/App5.java
@@ -0,0 +1,41 @@
+package edu.buet.cse.jpa.ch01.v1.driver;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import edu.buet.cse.jpa.ch01.v1.Vehicle;
+
+public class App5 {
+  public static void main(String... args) {
+    EntityManagerFactory factory = null;
+    EntityManager em = null;
+
+    try {
+      factory = Persistence.createEntityManagerFactory("jpa-ch01-v1");
+      em = factory.createEntityManager();
+
+      TypedQuery<Vehicle> query = em.createQuery("SELECT v FROM Vehicle v WHERE v.vin = ?1", Vehicle.class);
+      query.setParameter(1, "v12345");
+
+      Vehicle vehicle = query.getSingleResult();
+
+      em.getTransaction().begin();
+      em.remove(vehicle);
+      em.getTransaction().commit();
+
+      System.out.printf("%s has been deleted successfully%n", vehicle);
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
+    } finally {
+      if (em != null) {
+        em.close();
+      }
+
+      if (factory != null) {
+        factory.close();
+      }
+    }
+  }
+}
