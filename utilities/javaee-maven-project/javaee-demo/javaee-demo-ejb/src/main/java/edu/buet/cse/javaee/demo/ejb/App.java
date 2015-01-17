5eebf96309c72e9327040aedfeac9aa23787bfa7
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-01-17 19:44:40 +1100

    added a multi-module maven project that can serve as the basis of generating an ear file

---
+++ utilities/javaee-maven-project/javaee-demo/javaee-demo-ejb/src/main/java/edu/buet/cse/javaee/demo/ejb/App.java
@@ -0,0 +1,13 @@
+package edu.buet.cse.javaee.demo.ejb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ utilities/javaee-maven-project/javaee-demo/javaee-demo-web/src/main/java/edu/buet/cse/javaee/demo/web/App.java
@@ -0,0 +1,13 @@
+package edu.buet.cse.javaee.demo.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
