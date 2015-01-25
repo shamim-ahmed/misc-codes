9de6409134018f63b768fddbbfe5965624269d15
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-01-25 20:20:09 +1100

    added a new project to demonstrate various features of JPA

---
+++ java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/driver/App.java
@@ -0,0 +1,44 @@
+package edu.buet.cse.jpa.ch02.v1.driver;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import edu.buet.cse.jpa.ch02.v1.model.Address;
+
+public class App {
+  public static void main(String... args) {
+    EntityManagerFactory emFactory = null;
+    EntityManager entityManager = null;
+    
+    try {
+      emFactory = Persistence.createEntityManagerFactory("jpa_pu");
+      entityManager = emFactory.createEntityManager();
+      
+      Address address = new Address();
+      address.setStreet("8 Wishart Street");
+      address.setCity("Sydney");
+      address.setZip("2122");
+      address.setState("NSW");
+      address.setCountry("AU");
+      address.setResidential(true);
+      
+      entityManager.getTransaction().begin();
+      entityManager.persist(address);
+      entityManager.getTransaction().commit();
+      
+      System.out.printf("The saved address is: %s%n", address);
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
+      entityManager.getTransaction().rollback();
+    } finally {
+      if (entityManager != null) {
+        entityManager.close();
+      }
+      
+      if (emFactory != null) {
+        emFactory.close();
+      }
+    }
+  }
+}
---
+++ java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/model/Address.java
@@ -0,0 +1,119 @@
+package edu.buet.cse.jpa.ch02.v1.model;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
+/**
+ * This class demonstrates the following:
+ * <ul>
+ * <li>Transient annotation</li>
+ * <li>Use of AccessType.PROPERTY</li>
+ * </ul>
+ * 
+ * @author shamim
+ *
+ */
+@Entity
+@Table(name = "ADDRESS")
+@Access(AccessType.PROPERTY)
+public class Address {
+  private Long id;
+  private String street;
+  private String city;
+  private String state;
+  private String zip;
+  private String country;
+  private Integer version;
+  private boolean isResidential;
+
+  // NOTE: When you specify GenerationType.IDENTITY, your database must be able to auto-generate values.
+  // For MySQL, this is enabled by specifying AUTO_INCREMENT in column definition
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", updatable = false)
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Column(name = "STREET", nullable = false)
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  @Column(name = "CITY")
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  @Column(name = "STATE", nullable = false)
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  @Column(name = "ZIP", length = 4, nullable = false)
+  public String getZip() {
+    return zip;
+  }
+
+  public void setZip(String zip) {
+    this.zip = zip;
+  }
+
+  @Column(name = "COUNTRY", nullable = false)
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  @Version
+  @Column(name = "VERSION", nullable = false)
+  public Integer getVersion() {
+    return version;
+  }
+
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  @Transient
+  public boolean isResidential() {
+    return isResidential;
+  }
+
+  public void setResidential(boolean isResidential) {
+    this.isResidential = isResidential;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[id = %d, street = %s, city = %s, state = %s, country = %s]", id, street, city, state,
+        country);
+  }
+}
