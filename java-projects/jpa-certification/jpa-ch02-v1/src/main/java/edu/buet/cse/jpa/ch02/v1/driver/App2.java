16fb73d8c1a6ab05bbec9b113953cdffa53e37bf
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-01-26 18:49:56 +1100

    updated the exception handling mechanism in all driver classes

--- java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/driver/App2.java
+++ java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/driver/App2.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import edu.buet.cse.jpa.ch02.v1.model.Book;
@@ -32,8 +33,12 @@ public class App2 {
     } catch (Exception ex) {
       ex.printStackTrace(System.err);
       
-      if (entityManager != null && entityManager.getTransaction() != null) {
-        entityManager.getTransaction().rollback();
+      if (entityManager != null) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        if (transaction != null && transaction.isActive()) {
+          transaction.rollback();
+        }
       }
     } finally {
       if (entityManager != null) {
--- java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/driver/App5.java
+++ java-projects/jpa-certification/jpa-ch02-v1/src/main/java/edu/buet/cse/jpa/ch02/v1/driver/App5.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import edu.buet.cse.jpa.ch02.v1.model.PreferredCustomer;
@@ -28,8 +29,12 @@ public class App5 {
     } catch (Exception ex) {
       ex.printStackTrace(System.err);
       
-      if (entityManager != null && entityManager.getTransaction() != null) {
-        entityManager.getTransaction().rollback();
+      if (entityManager != null) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        if (transaction != null && transaction.isActive()) {
+          transaction.rollback();
+        }
       }
     } finally {
       if (entityManager != null) {
