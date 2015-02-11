abbd6bcdc783ff515498aa9c0dc8eb1c179b6880
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-02-12 00:25:27 +1100

    input parsing is complete for uva 162

--- acm-problems/acm-prob162/src/main/java/Main.java
+++ acm-problems/acm-prob162/src/main/java/Main.java
@@ -1,69 +1,101 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
   private static final String INPUT_TERMINATOR = "#";
-  
+
   private static class Card {
     private char suite;
     private char rank;
-   
-    public Card(char suite, char rank) {
+
+    public char getSuite() {
+      return suite;
+    }
+
+    public void setSuite(char suite) {
       this.suite = suite;
+    }
+
+    public char getRank() {
+      return rank;
+    }
+
+    public void setRank(char rank) {
       this.rank = rank;
     }
-    
+
     public boolean isFaceCard() {
       return rank == 'J' || rank == 'Q' || rank == 'K' || rank == 'A';
     }
-    
+
     @Override
     public String toString() {
       return String.format("%c%c", suite, rank);
     }
   }
-  
+
+  private static final Card[][] DECK_OF_CARDS = new Card[4][13];
+
+  static {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 13; j++) {
+        DECK_OF_CARDS[i][j] = new Card();
+      }
+    }
+  }
+
   public static void main(String... args) {
     Scanner scanner = new Scanner(System.in);
-    
+
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine().trim();
-      
+
       if (line.equals(INPUT_TERMINATOR)) {
         break;
       }
-      
+
       // we have already read the first line of the deck
-      List<Card> cardList = new ArrayList<>();
-      parseLine(line, cardList);
-      
+      parseLine(line, 0);
+
       // let us read the next three lines of the deck
-      for (int i = 0; i < 3; i++) {
+      for (int i = 1; i < 4; i++) {
         line = scanner.nextLine().trim();
-        parseLine(line, cardList);
+        parseLine(line, i);
       }
-      
+
       // at this point, we have read the whole deck. So we can start the game.
-      playWithDeck(cardList);
+      playWithDeck();
     }
-    
+
     scanner.close();
   }
-  
-  private static void parseLine(String line, List<Card> cardList) {
+
+  private static void parseLine(String line, int i) {
     StringTokenizer tokenizer = new StringTokenizer(line, " ", false);
-    
+    int j = 0;
+
     while (tokenizer.hasMoreTokens()) {
       String token = tokenizer.nextToken();
       char s = token.charAt(0);
       char r = token.charAt(1);
-      cardList.add(new Card(s, r));
+      Card card = DECK_OF_CARDS[i][j];
+      card.setSuite(s);
+      card.setRank(r);
+      j++;
     }
   }
-  
-  private static void playWithDeck(List<Card> cardList) {
-    
+
+  private static void playWithDeck() {
+    // TODO implement it properly
+    printDeck();
+  }
+
+  private static void printDeck() {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 13; j++) {
+        System.out.printf("%s ", DECK_OF_CARDS[i][j]);
+      }
+      System.out.println();
+    }
   }
 }
