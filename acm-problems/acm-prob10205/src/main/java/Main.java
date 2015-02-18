a05c1789d47cac82bca3481a1695c04f3404661b
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-02-18 23:39:01 +1100

    added a skeletal project for uva problem 10205

---
+++ acm-problems/acm-prob10205/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+
+
+public class Main {
+  public static void main(String... args) {
+    processInput(System.in, System.out);
+  }
+  
+  public static void processInput(InputStream inputStream, PrintStream outputStream) {
+    
+  }
+}
