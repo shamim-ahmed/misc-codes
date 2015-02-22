29fc174ac29196fb6f9835353fec9687268b9e01
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-02-22 13:00:14 +1100

    now the input values are sorted

--- acm-input-generators/generator-11678/src/main/java/edu/buet/cse/generator_11678/App.java
+++ acm-input-generators/generator-11678/src/main/java/edu/buet/cse/generator_11678/App.java
@@ -1,15 +1,16 @@
 package edu.buet.cse.generator_11678;
 
 import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.Random;
 
 public class App {
   private static final String LINE_SEPARATOR = "\n";
   private static final String SPACE = " ";
-  
-  private static final int NUMBER_OF_CASES = 2;
-  private static final int MAX_NO_OF_CARDS = 100;
-  private static final int MAX_CARD_VALUE = 1000;
+
+  private static final int NUMBER_OF_CASES = 16;
+  private static final int MAX_NO_OF_CARDS = 1000;
+  private static final int MAX_CARD_VALUE = 10000;
 
   public static void main(String[] args) {
     generate(System.out);
@@ -29,28 +30,32 @@ public class App {
 
       for (int j = 0; j < aliceValues.length; j++) {
         aliceValues[j] = 1 + randomGenerator.nextInt(MAX_CARD_VALUE);
-        resultBuilder.append(aliceValues[j]);
-
-        if (j < aliceValues.length - 1) {
-          resultBuilder.append(SPACE);
-        }
       }
-
-      resultBuilder.append(LINE_SEPARATOR);
 
       for (int j = 0; j < bettyValues.length; j++) {
         bettyValues[j] = 1 + randomGenerator.nextInt(MAX_CARD_VALUE);
-        resultBuilder.append(bettyValues[j]);
-
-        if (j < bettyValues.length - 1) {
-          resultBuilder.append(SPACE);
-        }
       }
 
-      resultBuilder.append(LINE_SEPARATOR);
+      Arrays.sort(aliceValues);
+      Arrays.sort(bettyValues);
+
+      appendValues(aliceValues, resultBuilder);
+      appendValues(bettyValues, resultBuilder);
     }
 
     resultBuilder.append(0).append(SPACE).append(0);
     outputStream.print(resultBuilder.toString());
+  }
+
+  private static void appendValues(int[] values, StringBuilder resultBuilder) {
+    for (int j = 0; j < values.length; j++) {
+      resultBuilder.append(values[j]);
+
+      if (j < values.length - 1) {
+        resultBuilder.append(SPACE);
+      }
+    }
+
+    resultBuilder.append(LINE_SEPARATOR);
   }
 }
