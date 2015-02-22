f617845d397e564e50d29425005cb7cb36efb82d
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-02-22 12:30:35 +1100

    added an input generator program for uva 11678

---
+++ acm-input-generators/generator-11678/src/main/java/edu/buet/cse/generator_11678/App.java
@@ -0,0 +1,55 @@
+package edu.buet.cse.generator_11678;
+
+import java.io.PrintStream;
+import java.util.Random;
+
+public class App {
+  private static final String LINE_SEPARATOR = "\n";
+  private static final String SPACE = " ";
+  
+  private static final int NUMBER_OF_CASES = 2;
+  private static final int MAX_NO_OF_CARDS = 10000;
+  private static final int MAX_CARD_VALUE = 100000;
+
+  public static void main(String[] args) {
+    generate(System.out);
+  }
+
+  private static void generate(PrintStream outputStream) {
+    StringBuilder resultBuilder = new StringBuilder();
+    Random randomGenerator = new Random();
+
+    for (int i = 0; i < NUMBER_OF_CASES; i++) {
+      final int m = 1 + randomGenerator.nextInt(MAX_NO_OF_CARDS);
+      final int n = 1 + randomGenerator.nextInt(MAX_NO_OF_CARDS);
+
+      resultBuilder.append(m).append(SPACE).append(n).append(LINE_SEPARATOR);
+      int[] aliceValues = new int[m];
+      int[] bettyValues = new int[n];
+
+      for (int j = 0; j < aliceValues.length; j++) {
+        aliceValues[j] = 1 + randomGenerator.nextInt(MAX_CARD_VALUE);
+        resultBuilder.append(aliceValues[j]);
+
+        if (j < aliceValues.length - 1) {
+          resultBuilder.append(SPACE);
+        }
+      }
+
+      resultBuilder.append(LINE_SEPARATOR);
+
+      for (int j = 0; j < bettyValues.length; j++) {
+        bettyValues[j] = 1 + randomGenerator.nextInt(MAX_CARD_VALUE);
+        resultBuilder.append(bettyValues[j]);
+
+        if (j < bettyValues.length - 1) {
+          resultBuilder.append(SPACE);
+        }
+      }
+
+      resultBuilder.append(LINE_SEPARATOR);
+    }
+
+    outputStream.print(resultBuilder.toString());
+  }
+}
