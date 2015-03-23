3382220de83d7aa1ce19d4a0c14d576f32a32894
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-03-23 23:04:13 +1100

    reformatted the script and made it executable

--- utilities/bash-scripts/iteration.sh
+++ utilities/bash-scripts/iteration.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
 # simple iteration
-for i in {1..47}; do echo $i; done
+for i in {1..47}
+do
+  echo $i
+done
 
 # download files with names that follow a particular pattern
-for i in {1..47}; do  wget http://media.raagalahari.com/gallery/shabnakhan/shabnakhan${i}.jpg; done
+for i in {1..47}
+do
+  wget http://media.raagalahari.com/gallery/shabnakhan/shabnakhan${i}.jpg
+done
+
+# another iteration
+for i in {35..99}
+do
+  wget http://tamilgallery.com/images/Shabna-Khan-101{i}.jpg
+done
 
 # advanced formatting in shell
 for i in {1..138}
