d53353cba9713c39f899c1c4bdc5f864ce2b1bfa
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-04-19 17:57:33 +1000

    reformatted project source files so that only spaces are used for indentation

--- acm-problems-cpp/acm-prob10324/src/p10324.cpp
+++ acm-problems-cpp/acm-prob10324/src/p10324.cpp
@@ -8,72 +8,72 @@ int MAX_LENGTH = 1000000;
 bool checkInterval(char*, int, int, int);
 
 int main() {
-	string line;
-	char input[MAX_LENGTH];
-	int count = 0;
+  string line;
+  char input[MAX_LENGTH];
+  int count = 0;
 
-	while (getline(cin, line)) {
-		int len = line.length();
+  while (getline(cin, line)) {
+    int len = line.length();
 
-		if (len == 0) {
-			break;
-		}
+    if (len == 0) {
+      break;
+    }
 
-		count++;
-		strcpy(input, line.c_str());
+    count++;
+    strcpy(input, line.c_str());
 
-		getline(cin, line);
-		istringstream iss(line);
-		int n;
-		iss >> n;
-		cout << "Case " << count << ":" << endl;
+    getline(cin, line);
+    istringstream iss(line);
+    int n;
+    iss >> n;
+    cout << "Case " << count << ":" << endl;
 
-		for (int i = 0; i < n; i++) {
-			getline(cin, line);
-			istringstream indexStream(line);
-			int p, q;
-			indexStream >> p;
-			indexStream >> q;
+    for (int i = 0; i < n; i++) {
+      getline(cin, line);
+      istringstream indexStream(line);
+      int p, q;
+      indexStream >> p;
+      indexStream >> q;
 
-			bool result = checkInterval(input, len, p, q);
+      bool result = checkInterval(input, len, p, q);
 
-			if (result) {
-				cout << "Yes" << endl;
-			} else {
-				cout << "No" << endl;
-			}
-		}
-	}
+      if (result) {
+        cout << "Yes" << endl;
+      } else {
+        cout << "No" << endl;
+      }
+    }
+  }
 }
 
 bool checkInterval(char* inputChars, int arrayLength, int p, int q) {
-	if (p >= arrayLength || q >= arrayLength) {
-		return false;
-	}
+  if (p >= arrayLength || q >= arrayLength) {
+    return false;
+  }
 
-	if (p == q) {
-		return true;
-	}
+  if (p == q) {
+    return true;
+  }
 
-	int start, end;
+  int start, end;
 
-	if (p < q) {
-		start = p;
-		end = q;
-	} else {
-		start = q;
-		end = p;
-	}
+  if (p < q) {
+    start = p;
+    end = q;
+  } else {
+    start = q;
+    end = p;
+  }
 
-	bool result = true;
-	char valueToCheck = inputChars[start];
+  bool result = true;
+  char valueToCheck = inputChars[start];
 
-	for (int i = start + 1; i <= end; i++) {
-		if (valueToCheck != inputChars[i]) {
-			result = false;
-			break;
-		}
-	}
+  for (int i = start + 1; i <= end; i++) {
+    if (valueToCheck != inputChars[i]) {
+      result = false;
+      break;
+    }
+  }
 
-	return result;
+  return result;
 }
