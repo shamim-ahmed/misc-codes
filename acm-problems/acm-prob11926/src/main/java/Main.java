c8710852c2bfa97ad6201c452b3bb123cace3790
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-03-25 01:18:40 +1100

    added incomplete solution to uva problem 11926

---
+++ acm-problems/acm-prob11926/src/main/java/Main.java
@@ -0,0 +1,88 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class Main {
+  private static final int MAX = 1000000;
+  private static final String CONFLICT_STR = "CONFLICT";
+  private static final String NO_CONFLICT_STR = "NO CONFLICT";
+
+  public static void main(String... args) {
+    processInput(System.in, System.out);
+  }
+
+  public static void processInput(InputStream inStream, PrintStream outStream) {
+    Scanner scanner = new Scanner(inStream);
+    StringBuilder resultBuilder = new StringBuilder();
+
+    while (scanner.hasNextInt()) {
+      int numberOfOneTimeTasks = scanner.nextInt();
+      int numberOfRepeatingTasks = scanner.nextInt();
+
+      if (numberOfOneTimeTasks == 0 && numberOfRepeatingTasks == 0) {
+        break;
+      }
+
+      BitSet schedule = new BitSet(MAX + 1);
+      boolean conflictFound = false;
+
+      for (int i = 0; i < numberOfOneTimeTasks; i++) {
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        if (!conflictFound) {
+          conflictFound = scanInterval(schedule, start, end);
+        }
+      }
+
+      for (int i = 0; i < numberOfRepeatingTasks; i++) {
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int repeatInterval = scanner.nextInt();
+        int delta = end - start;
+
+        for (int p = start; p <= MAX && !conflictFound; p += repeatInterval) {
+          int q = p + delta;
+          conflictFound = scanInterval(schedule, p, q);
+        }
+      }
+
+      if (conflictFound) {
+        resultBuilder.append(String.format("%s%n", CONFLICT_STR));
+      } else {
+        resultBuilder.append(String.format("%s%n", NO_CONFLICT_STR));
+      }
+    }
+    scanner.close();
+    outStream.print(resultBuilder.toString());
+  }
+
+  /*
+   * returns true if a conflict has been found in the interval
+   */
+  private static boolean scanInterval(BitSet schedule, int start, int end) {
+    int delta = end - start;
+
+    // interval of length one needs to be handled carefully
+    if (delta == 1 && schedule.get(start) && schedule.get(end)) {
+      return true;
+    }
+
+    // allow overlapping at start
+    schedule.set(start);
+
+    for (int j = start + 1; j < end; j++) {
+      if (schedule.get(j)) {
+        return true;
+      }
+
+      schedule.set(j);
+    }
+
+    // allow overlapping at end
+    schedule.set(end);
+
+    return false;
+  }
+}
